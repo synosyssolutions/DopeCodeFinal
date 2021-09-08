00e3c1fdd8423a386aea31815a25310127caf2ab
Piyush <p4pp.piyush@gmail.com>
2021-09-08 22:40:56 +0530

    home page first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeMiddleComponent } from './home-middle/home-middle.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeMiddleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
